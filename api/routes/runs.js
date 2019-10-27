98334d7bdc0e25b0fbb310408c3d9176363a3522
Connor Monahan <cmonahan@innovationdx.com>
2019-10-26 20:06:23 -0500

    Add CSV export, make every function async/await

--- api/routes/runs.js
+++ api/routes/runs.js
@@ -1,69 +1,11 @@
-const { Writable, Transform } = require("stream");
+const { Transform } = require("stream");
 var express = require('express');
 const parser = require('../parser');
+const stringify = require('csv-stringify')
 var router = express.Router();
-const { promisify } = require('util');
-const crypto = require('crypto')
 
 
-// Get listing of all runs
-router.get('/', function (req, res) {
-	/** @type {(key: string) => Promise<string[]>} */
-	req.db.query("SELECT id, location, description, type, runofday, start AS date, end FROM datarunmeta ORDER BY start ASC")
-		.then((rows) => {
-			res.status(200).send(rows)
-		})
-		.catch((error) => {
-			res.status(500).send({ error })
-		});
-});
-
-// Upload a new run log file
-router.post('/', function (req, res) {
-	// create the new run ID
-	req.db.query(
-		"INSERT INTO datarunmeta (location, runofday) VALUES (?, ?)",
-		[req.fields.location, req.fields.runofday])
-		.then((result) => {
-			let id = result.insertId;
-			// queue up processing
-			parser.importFile(req.files.file.path, id, req.db);
-			// let the client know we're processing now
-			res.status(202).location(`/api/runs/${id}`).send({ id });
-		})
-		.catch((error) => {
-			console.log(error)
-			res.status(500).send({ error });
-		});
-});
-
-router.get("/processing/:runId", (req, res) => {
-	let id = parseInt(req.params.runId);
-	// check if its still processing.
-	let job = parser.queue.checkStatus(id);
-	if (job) {
-		res.status(200).send({ id: id, status: job.status, progress: job.count / job.total});
-	} else {
-		res.status(404).send({ id });
-	}
-})
-
-function combineDP(data, varmap) {
-	// combine data points with the same time
-	let dp = { time: 0 };
-	let newdata = [];
-	for (row of data) {
-		if (dp.time != 0 && row.time.getTime() !== dp.time.getTime()) {
-			// push old data to array first
-			newdata.push(dp);
-			dp = { time: 0 };
-		}
-		dp.time = row.time;
-		dp[varmap[row.variable]] = row.value;
-	}
-	return newdata;
-}
-
+// Utility function to read all variables and build a map
 function readVars(db) {
 	return db.query("SELECT id, name FROM datavariables").then((varlist) => {
 		let varmap = {};
@@ -74,13 +16,25 @@ function readVars(db) {
 	})
 }
 
-
+// Input from SQL resultset, output as time/value objects
 class VariableExpander extends Transform {
-	constructor(varmap, trash) {
-		super({objectMode: true});
+	constructor(varmap) {
+		super({ objectMode: true });
 		this.varmap = varmap;
+	}
+	_transform(chunk, encoding, callback) {
+		let dp = { time: chunk.time };
+		dp[this.varmap[chunk.variable]] = chunk.value;
+		callback(null, JSON.stringify(dp));
+	}
+}
+
+// Input text, Output text wrapped in [], each line split by a comma
+class ArrayWrapper extends Transform {
+	constructor() {
+		super();
 		this.first = true;
-		this.trash = trash;
+		this.push("[");
 	}
 	_transform(chunk, encoding, callback) {
 		if (this.first) {
@@ -88,219 +42,263 @@ class VariableExpander extends Transform {
 		} else {
 			this.push(",");
 		}
-		let dp = {time: chunk.time};
-		dp[this.varmap[chunk.variable]] = chunk.value;
-		callback(null, JSON.stringify(dp));
+		callback(null, chunk);
 	}
 	_flush(callback) {
-		callback(null, this.trash);
+		callback(null, "]");
 	}
 }
 
-// Get details on a particular run
-router.get("/:runId", function (req, res) {
-	let id = parseInt(req.params.runId)
+// Input SQL resultset, Output table-style arrays
+class Tablify extends Transform {
+	constructor(variables, varmap, start) {
+		super({ objectMode: true });
+		// index into this.line
+		this.invvarmap = {};
+		this.start = start;
+		for (let i = 0; i < variables.length; i++) {
+			this.invvarmap[variables[i]] = i + 1;
+		}
+		let header = ["time"].concat(variables.map(varid => varmap[varid]));
+		this.push(header);
+		this.line = header.map(() => 0);
+		console.log(this.invvarmap);
+	}
+	_transform(chunk, encoding, callback) {
+		// time from the start at the beginning
+		this.line[0] = (chunk.time - this.start) / 1000;
+		// this variable's value in its place
+		this.line[this.invvarmap[chunk.variable]] = chunk.value;
+		callback(null, this.line.slice());
+	}
+}
 
-	let varmap;
-
-	readVars(req.db).then((map) => { varmap = map; return varmap; })
-		.then(() => req.db.query("SELECT location, description, type, runofday, start, end FROM datarunmeta WHERE id = ? LIMIT 1", [id]))
-		.then((rows) => {
-			if (rows.length !== 1) {
-				res.status(404).send({ error: "Run not found" })
-				return;
-			} 
-			let meta = rows[0]
-			// avoid hitting the DB if we don't have to
-			let etag = crypto.createHash('md5').update(JSON.stringify(meta)).digest("hex")
-			if (req.headers["if-none-match"] == etag) {
-				return res.sendStatus(304);
+/**
+ * Get a list of filters applicable to a data collection
+ * @param {{name: string, expression: string}[]} globalFilters 
+ * @param {{id: number, name: string}[]} globalVariables 
+ * @param {{id: number, name: string}[]} localVariables 
+ * @returns {{name: string, expression: string, required: number[]}[]}
+ */
+function getApplicableFilters(globalFilters, globalVariables, localVariables) {
+	let allVarNames = globalVariables.map((v) => v.name);
+	let presentVarNames = new Set(localVariables.map((v) => v.name));
+	// find string name set difference globalVariables \ localVariables
+	let nonpresentVarNames = allVarNames.filter((v) => !presentVarNames.has(v));
+	// remove filters that contain a nonpresent variable
+	let applicableFilters = globalFilters.filter((f) => {
+		for (let npvar of nonpresentVarNames) {
+			if (f.expression.includes(npvar)) {
+				return false;
 			}
-			meta.id = id;
-			meta.date = meta.start;
-			let placeholder = "XXXXXXXXXX";
-			meta.data = placeholder;
-			// start the stream
-			res.setHeader('Content-Type', 'application/json');
-			res.setHeader('Transfer-Encoding', 'chunked');
-			res.setHeader('ETag', etag);
-			res.setHeader('Cache-Control', 'public, max-age=7000000'); // remember it for 3 months
-			// send the metadata and opening array marker
-			res.write(JSON.stringify(meta).split(`"${placeholder}"`)[0] + "[");
-			// stream the rest of the data
-			return req.db.queryStream("SELECT `time`, `value`, `variable` from datapoints where `time` > ? and `time` < ? order by `time` ASC", [meta.start, meta.end])
-			.pipe(new VariableExpander(varmap, "]}"))
-			.pipe(res)
-		})
-		.catch((error) => {
-			console.error(error)
-			res.status(500).send({ error })
-		});
+		}
+		return true;
+	})
+	// store information about required variables in each remaining filter
+	for (let filter of applicableFilters) {
+		filter.required = [];
+		for (let variable of localVariables) {
+			if (filter.expression.includes(variable.name)) {
+				filter.required.push(variable.id);
+			}
+		}
+	}
 
+	return applicableFilters;
+}
+
+
+/**
+ * Create
+ */
+
+// Upload a new run log file
+router.post('/', async (req, res) => {
+	try {
+		// create the new run ID
+		let result = await req.db.query(
+			"INSERT INTO datarunmeta (location, runofday) VALUES (?, ?)",
+			[req.fields.location, req.fields.runofday]);
+		let id = result.insertId;
+		// queue up processing
+		parser.importFile(req.files.file.path, id, req.db);
+		// let the client know we're processing now
+		return res.status(202).location(`/api/runs/${id}`).send({ id });
+	} catch (error) {
+		console.log(error);
+		return res.status(500).send({ error: "Unhandled server error" });
+
+	}
 });
 
-router.get("/points/:start/:end/:sampleSize/:variables", (req, res) => {
-	let start = new Date(req.params.start);
-	let end = new Date(req.params.end);
-	let sample = parseInt(req.params.sampleSize);
-	let variables = req.params.variables.split(",").map(varstr => parseInt(varstr));
-
-	return readVars(req.db)
-	.then((varmap) => {
-		// avoid hitting the DB if we don't have to
-		let etag = start.toUTCString(); // in the future, compute the last data point in the day or something
-		if (new Date(req.headers["if-modified-since"]) >= start) {
-			return res.sendStatus(304);
-		}
-		// start the stream
-		res.setHeader('Content-Type', 'application/json');
-		res.setHeader('Transfer-Encoding', 'chunked');
-		res.setHeader('Last-Modified', etag);
-		res.setHeader('Cache-Control', 'public, max-age=7000000'); // remember it for 3 months
-		// send the metadata and opening array marker
-		res.write("[");
-		// stream the rest of the data
-		return req.db.queryStream("SELECT `time`, `value`, `variable` from datapoints USE INDEX (datapoints_UN) where time between ? AND ?  and variable in (" + variables.map(() => "?").join(",") + ") " + (sample == 0 ? "order by `time` ASC" : `ORDER BY RAND() LIMIT ${sample}`), [start, end].concat(variables))
-		.pipe(new VariableExpander(varmap, "]"))
-		.pipe(res)
-	})
-	.catch((error) => {
-		console.error(error)
-		res.status(500).send({ error })
-	});
+// Check uploaded run processing state
+router.get("/processing/:runId", (req, res) => {
+	let id = parseInt(req.params.runId);
+	// check if its still processing.
+	let job = parser.queue.checkStatus(id);
+	if (job) {
+		res.status(200).send({ id: id, status: job.status, progress: job.count / job.total });
+	} else {
+		res.status(404).send({ id });
+	}
 })
+
+/**
+ * Read
+ */
+
+
+// Get listing of all runs
+router.get('/', async (req, res) => {
+	try {
+		let rows = await req.db.query(
+			"SELECT id, location, description, type, runofday, start AS date, end FROM datarunmeta ORDER BY start ASC");
+		return res.status(200).send(rows);
+	} catch (error) {
+		console.log(error);
+		return res.status(500).send({ error: "Unhandled server error" });
+	}
+});
 
 // Read only the run's details and variables list
-router.get("/:runId/details", (req, res) => {
-	let id = parseInt(req.params.runId);
-	let meta = {};
-	let localvars = [];
-	let globalvars = [];
-	let globalfilters = [];
-	// read details
-	req.db.query("SELECT id, name FROM datavariables")
-	.then((vars) => { globalvars = vars; })
-	.then(() => req.db.query("SELECT name, expression FROM datafilters"))
-	.then((filters) => { globalfilters = filters; })
-	.then(() => req.db.query("SELECT location, description, type, runofday, start, end FROM datarunmeta WHERE id = ? LIMIT 1", [id]))
-	.then((rows) => {
-		if (rows.length !== 1) {
-			return Promise.reject({ status: 404, message: "Run not found" });
+router.get("/:runId/details", async (req, res) => {
+	try {
+		let id = parseInt(req.params.runId);
+		// read information about this run
+		let runs = await req.db.query("SELECT location, description, type, runofday, start, end FROM datarunmeta WHERE id = ? LIMIT 1", [id]);
+		if (runs.length !== 1) {
+			return res.status(404).send({ error: "Run not found" })
 		}
-		meta = rows[0];
-	})
-	// get all variables applicable
-	.then(() => req.db.query("SELECT DISTINCT datavariables.id, datavariables.name FROM datapoints USE INDEX (datapoints_UN) INNER JOIN datavariables ON datavariables.id = datapoints.variable WHERE datapoints.time BETWEEN ? AND ?", [meta.start, meta.end]))
-	.then((vars) => {
+		let meta = runs[0];
+		// read general information
+		let globalVariables = await req.db.query("SELECT id, name FROM datavariables");
+		let globalFilters = await req.db.query("SELECT name, expression FROM datafilters");
+		// get all variables applicable
+		let localVariables = await req.db.query("SELECT DISTINCT datavariables.id, datavariables.name FROM datapoints USE INDEX (datapoints_UN) INNER JOIN datavariables ON datavariables.id = datapoints.variable WHERE datapoints.time BETWEEN ? AND ?", [meta.start, meta.end]);
 		// build a list of filters we can actually use for this run
-		localvars = vars;
-		let allVarNames = globalvars.map((v) => v.name);
-		let presentVarNames = new Set(localvars.map((v) => v.name));
-		let nonpresentVarNames = allVarNames.filter((v) => !presentVarNames.has(v));
-		let applicableFilters = globalfilters.filter((f) => {
-			for (let npvar of nonpresentVarNames) {
-				if (f.expression.includes(npvar)) {
-					// console.log(`Dropping ${f.name} missing ${npvar}`)
-					return false;
-				}
-			}
-			return true;
-		})
-		for (let filter of applicableFilters) {
-			filter.required = [];
-			for (let variable of localvars) {
-				if (filter.expression.includes(variable.name)) {
-					filter.required.push(variable.id);
-				}
-			}
+		let localFilters = getApplicableFilters(globalFilters, globalVariables, localVariables);
+		return res.status(200).send({ meta: meta, variables: localVariables, filters: localFilters });
+	} catch (error) {
+		console.log(error);
+		return res.status(500).send({ error: "Unhandled server error" });
+	}
+});
+
+// Read list of variables and filters for any abstract range
+router.get("/range/:start/:end/details", async (req, res) => {
+	try {
+		let start = new Date(req.params.start);
+		let end = new Date(req.params.end);
+		let meta = { location: "Unknown", start: start, end: end };
+		// read information about some run that day
+		let runs = await req.db.query("SELECT location FROM datarunmeta WHERE start BETWEEN ? AND ? LIMIT 1", [start, end]);
+		if (runs.length === 1) {
+			meta.location = runs[0].location;
 		}
-		res.status(200).send({ meta: meta, variables: vars, filters: applicableFilters });
-	})
-	.catch((error) => {
-		if (error.status) {
-			res.status(error.status).send({ error: error.message });
-		} else {
-			res.status(500).send({ error: error });
-		}
-	})
-})
+		// read general information
+		let globalVariables = await req.db.query("SELECT id, name FROM datavariables");
+		let globalFilters = await req.db.query("SELECT name, expression FROM datafilters");
+		// get all variables applicable
+		let localVariables = await req.db.query("SELECT DISTINCT datavariables.id, datavariables.name FROM datapoints USE INDEX (datapoints_UN) INNER JOIN datavariables ON datavariables.id = datapoints.variable WHERE datapoints.time BETWEEN ? AND ?", [meta.start, meta.end]);
+		// build a list of filters we can actually use for this run
+		let localFilters = getApplicableFilters(globalFilters, globalVariables, localVariables);
+		return res.status(200).send({ meta: meta, variables: localVariables, filters: localFilters });
+	} catch (error) {
+		console.log(error);
+		return res.status(500).send({ error: "Unhandled server error" });
+	}
+});
 
-router.get("/range/:start/:end", (req, res) => {
+// Read a sample of points for a range of time
+router.get("/points/:start/:end/:sampleSize/:variables\.:ext?", async (req, res) => {
+	try {
+		let start = new Date(req.params.start);
+		let end = new Date(req.params.end);
+		let sample = parseInt(req.params.sampleSize);
+		let variables = req.params.variables.split(",").map(varstr => parseInt(varstr));
 
-	let start = new Date(req.params.start);
-	let end = new Date(req.params.end);
-	let meta = {id: 0, runofday: 0, start: start, end: end, date: start, data: []};
-
-	return readVars(req.db)
-	.then((varmap) => {
+		let varmap = await readVars(req.db);
 		// avoid hitting the DB if we don't have to
 		let etag = start.toUTCString(); // in the future, compute the last data point in the day or something
 		if (new Date(req.headers["if-modified-since"]) >= start) {
 			return res.sendStatus(304);
 		}
-		let placeholder = "XXXXXXXXXX";
-		meta.data = placeholder;
 		// start the stream
-		res.setHeader('Content-Type', 'application/json');
+		if (req.params.ext === "csv") {
+			let filename = `${start.toISOString().replace(/:/g, "-")}_${variables.join(",")}.csv`;
+			res.setHeader('Content-Type', 'text/csv');
+			res.setHeader('Content-Disposition', `attachment; filename="${filename}"`)
+		} else {
+			res.setHeader('Content-Type', 'application/json');
+		}
 		res.setHeader('Transfer-Encoding', 'chunked');
 		res.setHeader('Last-Modified', etag);
 		res.setHeader('Cache-Control', 'public, max-age=7000000'); // remember it for 3 months
-		// send the metadata and opening array marker
-		res.write(JSON.stringify(meta).split(`"${placeholder}"`)[0] + "[");
 		// stream the rest of the data
-		return req.db.queryStream("SELECT `time`, `value`, `variable` from datapoints where `time` > ? and `time` < ? order by `time` ASC", [meta.start, meta.end])
-		.pipe(new VariableExpander(varmap, "]}"))
-		.pipe(res)
-	})
-	.catch((error) => {
-		console.error(error)
-		res.status(500).send({ error })
-	});
+		let s = req.db.queryStream("SELECT `time`, `value`, `variable` from datapoints USE INDEX (datapoints_UN) where time between ? AND ?  and variable in (" + variables.map(() => "?").join(",") + ") " + (sample == 0 ? "order by `time` ASC" : `ORDER BY RAND() LIMIT ${sample}`), [start, end].concat(variables));
+		if (req.params.ext === "csv") {
+			s = s.pipe(new Tablify(variables, varmap, start));
+			s = s.pipe(stringify());
+		} else {
+			s = s.pipe(new VariableExpander(varmap));
+			s = s.pipe(new ArrayWrapper());
+		}
+		s = s.pipe(res);
+	} catch (error) {
+		console.log(error);
+		return res.status(500).send({ error: "Unhandled server error" });
+	}
 })
 
-router.patch("/:runId", (req, res) => {
-	/** @type {(key: string, value: string) => Promise<>} */
-	let id = parseInt(req.params.runId);
-	if (isNaN(id) || id < 0) {
-		return res.status(400).send({ error: "Invalid ID" })
-	}
+/**
+ * Update
+ */
 
-	let actions = [];
-	if (req.fields.description) {
-		let description = req.fields.description;
-		actions.push(req.db.query("UPDATE datarunmeta SET description = ? WHERE id = ?", [description, id]));
+// Change run metadata
+router.patch("/:runId", async (req, res) => {
+	try {
+		let id = parseInt(req.params.runId);
+		if (isNaN(id) || id < 0) {
+			return res.status(400).send({ error: "Invalid ID" })
+		}
+
+		if (req.fields.description) {
+			let description = req.fields.description;
+			await req.db.query("UPDATE datarunmeta SET description = ? WHERE id = ?", [description, id]);
+		}
+		if (req.fields.location) {
+			let location = req.fields.location;
+			await req.db.query("UPDATE datarunmeta SET location = ? WHERE id = ?", [location, id]);
+		}
+		if (req.fields.type) {
+			let type = req.fields.type;
+			await req.db.query("UPDATE datarunmeta SET type = ? WHERE id = ?", [type, id]);
+		}
+		return res.status(200).send({ id });
+	} catch (error) {
+		console.log(error);
+		return res.status(500).send({ error: "Unhandled server error" });
 	}
-	if (req.fields.location) {
-		let location = req.fields.location;
-		actions.push(req.db.query("UPDATE datarunmeta SET location = ? WHERE id = ?", [location, id]));
-	}
-	if (req.fields.type) {
-		let type = req.fields.type;
-		actions.push(req.db.query("UPDATE datarunmeta SET type = ? WHERE id = ?", [type, id]));
-	}
-	Promise.all(actions)
-	.then((results) => {
-		res.status(200).send({ id });
-	})
-	.catch((error) => {
-		res.status(500).send({ error });
-	})
 })
+
+/**
+ * Delete
+ */
 
 // Delete a run
-router.delete("/:runId", (req, res) => {
-	let id = parseInt(req.params.runId);
-	if (isNaN(id) || id < 0) {
-		return res.status(400).send({ error: "Invalid ID" })
-	}
+router.delete("/:runId", async (req, res) => {
+	try {
+		let id = parseInt(req.params.runId);
+		if (isNaN(id) || id < 0) {
+			return res.status(400).send({ error: "Invalid ID" })
+		}
 
-	req.db.query("DELETE FROM datarunmeta WHERE id = ?", [id])
-		.then(() => {
-			res.status(200).send({ id });
-		})
-		.catch((error) => {
-			res.status(500).send({ error });
-		})
+		await req.db.query("DELETE FROM datarunmeta WHERE id = ?", [id]);
+		return res.status(200).send({ id });
+	} catch (error) {
+		console.log(error);
+		return res.status(500).send({ error: "Unhandled server error" });
+	}
 });
 
 module.exports = router;
