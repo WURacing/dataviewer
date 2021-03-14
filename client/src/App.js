d0e7f62349e99105a0b245d4f4bf9ea0c16cbd1e
BenRedgate <b.k.redgate@wustl.edu>
2021-03-14 19:16:53 -0400

    testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import RunTable from './Components/RunTable'
-function App() {
+// import RunTable from './Components/RunTable'
+// function App() {
+//   return (
+//     <div className="AppBar">
+//       <RunTable apiPath = "https://data.washuracing.com/api/v2/testing"></RunTable>     
+//     </div>
+//   );
+// }
+
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+export default function FixedContainer() {
   return (
-    <div className="AppBar">
-      <RunTable apiPath = "https://data.washuracing.com/api/v2/testing"></RunTable>     
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container fixed>
+        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
+      </Container>
+    </React.Fragment>
   );
 }
 
